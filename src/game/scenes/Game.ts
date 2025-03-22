ddfaf9710b21dd01ae80f4b633656754add2d7af
Emerson Max <emersonthemax@gmail.com>
2025-03-22 09:49:20 -0700

    Init commit react game moving guy

---
+++ src/game/scenes/Game.ts
@@ -0,0 +1,100 @@
+import { EventBus } from '../EventBus';
+import { Scene } from 'phaser';
+
+export class Game extends Scene
+{
+    camera: Phaser.Cameras.Scene2D.Camera;
+    background: Phaser.GameObjects.Image;
+    gameText: Phaser.GameObjects.Text;
+    player: any;
+    platforms: any;
+    cursors: any;
+
+    constructor ()
+    {
+        super('Game');
+    }
+    create ()
+    {
+        // Setup
+        this.camera = this.cameras.main;
+
+        this.background = this.add.image(512, 384, 'background');
+
+        // Platform logic
+        this.platforms = this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+
+        // Player logic
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        // Collision logic
+        this.physics.add.collider(this.player, this.platforms);
+
+        // Input
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Main
+        EventBus.emit('current-scene-ready', this);
+    }
+
+    update ()
+    {
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else
+        {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down)
+        {
+            this.player.setVelocityY(-330);
+        }
+    }
+
+    changeScene ()
+    {
+        this.scene.start('GameOver');
+    }
+}
